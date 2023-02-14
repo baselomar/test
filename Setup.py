8c13d63be73e799a5ba03d09c16279c2950e32eb
baselomar <125217474+baselomar@users.noreply.github.com>
2023-02-14 23:16:51 +0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,3 +4,21 @@ import os
 os.system("clear")
 os.system("cd && cd test")
 os.system("bash TOOL.sh")
+print "\033[1;97m║--\033[1;91m> \033[1;92m1.\033[1;97m Normal login"
+	print "\033[1;97m║--\033[1;91m> \033[1;92m2.\033[1;97m Tokens login"
+	print "\033[1;97m║--\033[1;91m> \033[1;91m0.\033[1;97m Exit"
+	print "\033[1;97m║"
+	login_method = raw_input("\033[1;97m╚═\033[1;91m>>> \033[1;97m")
+	if login_method =="":
+		print"\033[1;91m[!] Wrong input"
+		exit()
+	elif login_method =="1":
+		login()
+	elif login_method =="2":
+		fbtoken()
+	elif login_method =="0":
+		exit()
+	else:
+		print"\033[1;91m[!] Wrong input"
+		exit()
+
