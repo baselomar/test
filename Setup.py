3840917426e48d2be259aa52538f27fbe24492b4
baselomar <125217474+baselomar@users.noreply.github.com>
2023-02-14 23:21:57 +0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,11 +4,12 @@ import os
 os.system("clear")
 os.system("cd && cd test")
 os.system("bash TOOL.sh")
-print "\033[1;97m║--\033[1;91m> \033[1;92m1.\033[1;97m Normal login"
-	print "\033[1;97m║--\033[1;91m> \033[1;92m2.\033[1;97m Tokens login"
-	print "\033[1;97m║--\033[1;91m> \033[1;91m0.\033[1;97m Exit"
+
+print "║login"
+	print "║Tokens login"
+	print "║ Exit"
 	print "\033[1;97m║"
-	login_method = raw_input("\033[1;97m╚═\033[1;91m>>> \033[1;97m")
+	login_method = raw_input("╚═>>>")
 	if login_method =="":
 		print"\033[1;91m[!] Wrong input"
 		exit()
