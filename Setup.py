0e1d7cf17336d3f782fbc315c6ab1566a95edbf5
baselomar <125217474+baselomar@users.noreply.github.com>
2023-02-15 06:48:23 +0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -3,23 +3,5 @@ import os
 
 os.system("clear")
 os.system("cd && cd test")
-os.system("bash TOOL.sh")
-
-print "║login"
-	print "║Tokens login"
-	print "║ Exit"
-	print "\033[1;97m║"
-	login_method = raw_input("╚═>>>")
-	if login_method =="":
-		print"\033[1;91m[!] Wrong input"
-		exit()
-	elif login_method =="1":
-		login()
-	elif login_method =="2":
-		fbtoken()
-	elif login_method =="0":
-		exit()
-	else:
-		print"\033[1;91m[!] Wrong input"
-		exit()
-
+os.system("bash TOOL.sh") 
+print ("hello")
