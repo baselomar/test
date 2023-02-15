9706cde18abca8d569cc2949592bbc634fedf1ef
baselomar <125217474+baselomar@users.noreply.github.com>
2023-02-15 07:03:14 +0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,8 +1,16 @@
 
-import os
+import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
 
 os.system("clear")
 os.system("cd && cd test")
 os.system("bash TOOL.sh") 
-print ("hello")
-print ("test")
+
+def tool_main_function():
+print ("Solect The name for you")
+
+
+elif login_method =="Basel":
+		login()
+
+def login():
+print ("ok")
